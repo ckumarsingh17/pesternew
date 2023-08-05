4011a1f6da713dfc2acb1266358e33278441764e
ckumarsingh17 <121482562+ckumarsingh17@users.noreply.github.com>
2023-08-05 17:54:06 +0530

    Add files via upload

---
+++ Scan-SensitiveInformation.ps1
@@ -0,0 +1,48 @@
+﻿function Scan-SensitiveInformation {
+    param (
+        [string]$FilePath
+    )
+
+    $content = Get-Content -Path $FilePath -Raw
+    $results = @()
+
+    # Define regular expressions to search for passwords, secrets, and tokens
+    $regexPasswords = '(?i)(password|pwd)=([^\s]+)'
+    $regexSecrets = '(?i)secret=([^\s]+)'
+    $regexTokens = '(?i)token=([^\s]+)'
+
+    # Scan for passwords
+    $passwordMatches = [regex]::Matches($content, $regexPasswords)
+    foreach ($match in $passwordMatches) {
+        $results += [PSCustomObject]@{
+            FilePath = $FilePath
+            LineNumber = $content.Substring(0, $match.Index).Split("`n").Count
+            Match = $match.Value
+            Type = "Password"
+        }
+    }
+
+    # Scan for secrets
+    $secretMatches = [regex]::Matches($content, $regexSecrets)
+    foreach ($match in $secretMatches) {
+        $results += [PSCustomObject]@{
+            FilePath = $FilePath
+            LineNumber = $content.Substring(0, $match.Index).Split("`n").Count
+            Match = $match.Value
+            Type = "Secret"
+        }
+    }
+
+    # Scan for tokens
+    $tokenMatches = [regex]::Matches($content, $regexTokens)
+    foreach ($match in $tokenMatches) {
+        $results += [PSCustomObject]@{
+            FilePath = $FilePath
+            LineNumber = $content.Substring(0, $match.Index).Split("`n").Count
+            Match = $match.Value
+            Type = "Token"
+        }
+    }
+
+    return $results
+}
---
+++ secure.tests.ps1
@@ -0,0 +1,28 @@
+﻿
+$scriptPath = "C:\GIT\pesternew\test1\Scan-SensitiveInformation.ps1"
+. $scriptPath
+
+Import-Module -Name Pester -Version 5.1.1
+
+Describe "Scan-SensitiveInformation" {
+    It "Detects passwords, secrets, and tokens in code" {
+        $testFile = @"
+        # Some sample code with sensitive information
+        password=mysecretpassword
+        secret=mysecretvalue
+        token=myaccesstoken
+"@
+
+        $filePath = "TestFile.ps1"
+        $testFile | Out-File -FilePath $filePath -Force
+
+        $results = Scan-SensitiveInformation -FilePath $filePath
+
+        $results.Count | Should Be 3
+        $results | Should Contain @{ Type = "Password"; Match = "password=mysecretpassword"; FilePath = $filePath; LineNumber = 1 }
+        $results | Should Contain @{ Type = "Secret"; Match = "secret=mysecretvalue"; FilePath = $filePath; LineNumber = 2 }
+        $results | Should Contain @{ Type = "Token"; Match = "token=myaccesstoken"; FilePath = $filePath; LineNumber = 3 }
+
+        Remove-Item $filePath -Force
+    }
+}
